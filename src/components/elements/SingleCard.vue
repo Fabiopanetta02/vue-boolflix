8a197f7dbff06b93a2bb8664f995167f7de02f54
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-07-28 14:54:58 +0200

    Added images films and series

--- src/components/elements/SingleCard.vue
+++ src/components/elements/SingleCard.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h4>{{info.title || info.name}}</h4>
+    <img v-if="!info.poster_path" src="https://ancasadental.com/wp-content/uploads/2020/09/dentalia-placeholder-img-4-750x750.jpg" alt="`${info.original_name}`" class="default-img">
+    <img v-else :src="`https://image.tmdb.org/t/p/w342/${info.poster_path}`" alt="`${info.original_name}`">
     <h5>{{info.original_title || info.original_name}}</h5>
     <lang-flag :iso="`${info.original_language}`"/>
     <p>{{info.vote_average}}</p>
@@ -22,4 +24,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.default-img{
+  width: 342px;
+  height: 513px;
+}
 </style>
